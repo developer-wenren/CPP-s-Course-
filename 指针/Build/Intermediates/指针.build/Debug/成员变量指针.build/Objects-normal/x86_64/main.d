8fc5ef4591c9060e48c7accf7eaa2ce301a5ae5b
wrcj <wrcj12138@163.com>
2015-03-19 22:40:25 +0800

    rename

---
+++ 指针/Build/Intermediates/指针.build/Debug/成员变量指针.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/zjsruxxxy3/Desktop/C++/指针/成员变量指针/main.cpp
---
+++ 指针/成员变量指针/main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  成员函数指针
+//
+//  Created by zjsruxxxy3 on 15/3/19.
+//  Copyright (c) 2015年 C++. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Date
+{
+    
+public:
+    int year;
+    int month;
+    int day;
+    
+    Date(int year,int month,int day):year(year),month(month),day(day)
+    {
+        this->year = year;
+        this->month = month;
+        this->day = day;
+        
+    }
+    
+    void show()
+    {
+        cout<<year<<" "<<month<<" "<<day<<endl;
+        
+    }
+    
+    void print(int Date::*p)
+    {
+        cout<<this->*p;
+        
+    }
+    
+};
+
+
+int main(int argc, const char * argv[])
+{
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    Date date(11,22,33);
+    
+    date.show();
+    
+    date.print(&Date::month);   // int Date:: *p = &Date::month
+    
+    return 0;
+}
