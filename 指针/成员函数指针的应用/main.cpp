4afd60f0c267ff148c01409e68e5a03341fff6b9
Wrcj_12138 <wrcj12138@163.com>
2015-03-21 13:33:52 +0800

    成员函数指针的修正

--- 指针/成员函数指针的应用/main.cpp
+++ 指针/成员函数指针的应用/main.cpp
@@ -8,17 +8,45 @@
 
 #include <iostream>
 
-class Person{
+class Person
+{
     
+};
+
+
+class Button;
+
+#define function_seletcor(sel)  (SEL)(&sel)
+
+typedef void (Person::*SEL)(Button *);
+
+class Button
+{
+    Person *target;
+    
+    SEL sel;
+    
+public:
+    int tag;
+    Button(Person *target,SEL sel):target(target),sel(sel)
+    {
+        
+    }
+    
+    void touch()
+    {
+        (target->*sel)(this); // 对象指针变量使用成员函数指针调用函数的方式，以免为* + sel()    // 传对象自己
+
+    }
 };
 
 class Hero:public Person  // 1.为何要public 继承 ？
 {
     
 public:
-    void move()
+    void move(Button *button)
     {
-        std::cout<<"英雄移动"<<std::endl;
+        std::cout<<"英雄移动"<< button->tag << std::endl;
         
     }
     
@@ -27,32 +55,9 @@ public:
 class Enemy: public Person
 {
 public:
-    void move()
+    void move(Person *sender)
     {
         std::cout<<"敌人移动"<<std::endl;
-        
-    }
-};
-
-#define function_seletcor(SEL)  SELT(&SEL)
-
-typedef void (Person::*SELT)();
-
-class Button
-{
-    Person *target;
-    
-    SELT sel;
-    
-public:
-    Button(Person *target,SELT sel):target(target),sel(sel)
-    {
-        
-    }
-    
-    void touch()
-    {
-        (target->*sel)(); // 对象指针变量使用成员函数指针调用函数的方式，以免为* + sel()
         
     }
 };
@@ -60,19 +65,23 @@ public:
 
 int main(int argc, const char * argv[])
 {
-
     // insert code here...
     
     std::cout << "Hello, Worldddd!\n";
     
     Hero *hero = new Hero;
     
+    std::cout<<hero<<std::endl;
+
     Enemy *enemy = new Enemy;
 
-    Button *button = new Button(enemy,(SELT)&Enemy::move); // 2.为何要强转？
+    Button *button = new Button(hero,(SEL)&Hero::move); // 2.为何要强转？
     
     Button *button2 = new Button(hero,function_seletcor(Hero::move));
-
+    
+    button->tag = 101;
+    button2->tag = 120;
+    
     button->touch();
     
     button2->touch();
