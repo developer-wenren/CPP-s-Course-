2184b8441fe30da592579eb83cb6332cfe1df70c
wrcj <wrcj12138@163.com>
2015-04-04 08:14:19 +0800

    +,-,输入，输出运算符的重载

--- 操作运算符/操作运算符/main.cpp
+++ 操作运算符/操作运算符/main.cpp
@@ -14,6 +14,7 @@ class SpecialNum
 private:
     int a;
     int b;
+    
 public:
     SpecialNum(int a,int b):a(a),b(b)
     {
@@ -30,10 +31,25 @@ public:
         return d ;
     }
     
-    // 友元函数（全局） 给自定义类写 operator<< 的方法
+    
+    // 友元函数（全局） 给自定义类写 cout<< 的方法/ cout--ostream对象，单例不让创建对象（cout输出运算符的重载）
    friend  ostream& operator<<(ostream &o,SpecialNum sp)
     {
         return o<<sp.a<<"/"<<sp.b;
+    }
+    
+    friend istream& operator>>(istream &i,SpecialNum &sp)
+    {
+        char ch;
+        
+        return i>>sp.a>>ch>>sp.b;
+        
+    }
+    
+    SpecialNum operator-(SpecialNum &sp)
+    {
+        return SpecialNum(this->a*sp.b-this->b*sp.a,this->b*sp.b);
+        
     }
 
 };
@@ -51,9 +67,22 @@ int main(int argc, const char * argv[])
 //    cout<<sp;
     SpecialNum sp3 = sp2+sp;
 //    cout<<(sp+sp2);
-    cout<<sp3;
+//    cout<<sp3;
     
-
+//    SpecialNum a;
+    
+#pragma mark cin<< 输入符号的重载
+//    cout<<sp;
+    
+//    cin>>sp;
+    
+//    cout<<sp<<endl;
+    
+    SpecialNum sp4 = sp - sp2;
+    
+    cout<<sp4<<endl;
+    
+    cout<<sp3<<endl;
     
     
     
