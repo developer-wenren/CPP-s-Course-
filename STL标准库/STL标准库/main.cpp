fdf024f4a822353b109d7d30c5a1908dc8f80632
wrcj <wrcj12138@163.com>
2015-04-05 10:22:11 +0800

    标准库

---
+++ STL标准库/STL标准库/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  STL标准库
+//
+//  Created by zjsruxxxy3 on 15/4/5.
+//  Copyright (c) 2015年 C++. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
